e354675d2ceb97a46f47908feabe5040af388c06
lkwsoul <lkwsoul@gmail.com>
2014-08-26 14:41:20 +0900

    Strategy UML

--- okdevtv_spring/strategy/src/main/java/com/okdevtv/pattern/before/CarDemo.java
+++ okdevtv_spring/strategy/src/main/java/com/okdevtv/pattern/before/CarDemo.java
@@ -1,9 +1,11 @@
 package com.okdevtv.pattern.before;
 
 public class CarDemo {
+  
+  static Car car;
 
   public static void main(String [] args){
-    Car car = new Car();
+    car = new Car();
     car.급정거(3);
   }
 }
