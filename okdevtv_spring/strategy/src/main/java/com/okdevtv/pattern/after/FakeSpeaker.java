03b7ab2c8ceb82355c3b80f5d5259d2ab7aca680
lkwsoul <lkwsoul@gmail.com>
2014-08-26 14:32:25 +0900

    : 20140826_전략패턴
    Task-Url:

---
+++ okdevtv_spring/strategy/src/main/java/com/okdevtv/pattern/after/FakeSpeaker.java
@@ -0,0 +1,15 @@
+package com.okdevtv.pattern.after;
+
+public class FakeSpeaker implements Speaker {
+
+  String string;
+  
+  public String getString() {
+   return string;
+  }
+  
+  public void yell(int level) {
+    this.string = String.valueOf(level);
+  }
+  
+}
---
+++ okdevtv_spring/strategy/src/main/java/com/okdevtv/pattern/before/CarDemo.java
@@ -0,0 +1,9 @@
+package com.okdevtv.pattern.before;
+
+public class CarDemo {
+
+  public static void main(String [] args){
+    Car car = new Car();
+    car.급정거(3);
+  }
+}
