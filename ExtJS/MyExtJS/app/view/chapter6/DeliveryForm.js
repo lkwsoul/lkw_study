5d78124360e939f7733a7985ad6cf3358b830470
lkwsoul <lkwsoul@gmail.com>
2015-03-05 18:08:12 +0900

    Modify 20150305

--- ExtJS/MyExtJS/app/view/chapter6/DeliveryForm.js
+++ ExtJS/MyExtJS/app/view/chapter6/DeliveryForm.js
@@ -74,12 +74,31 @@ Ext.define('ext5.view.chapter6.DeliveryForm',{
               triggerAction: 'query',                                     // 트리거를 클릭했을 때의 처리를 정의함.
               store: remoteJsonStore,                                     // 스토어 설정
               listConfig: {                                               // listConfig 재정의
-                getInnerTpl: function (displayField){                     
+                getInnerTpl: function (displayField) {                     
                   return '<div data-qtip="{fullName}">' +
                          '<div class="combo">{zipcode}</div>' +
                          '<div class="combo-address">{address}</div>' +
                          '</div>';
                 }
+              },
+              listeners: {
+                select: function(combo, records) {                                    // 콤보박스의 select 이벤트 리스닝
+                  var //zipcode = records[0].get('zipcode').split('-'),
+                      zipcode = records.get('zipcode').split('-'),                    // 선택한 코드의 '-'문자열로 나누어 배열로 전달함
+                      //address = records[0].get('address'),
+                      address = records.get('address'),                               // 주소정보 가져옴
+                      zipcodeField = this.query('[name=zipcode1],[name=zipcode2]'),   // this는 콤보박스를 의미
+                                                                                      // "socope: this"로 인해 찾는 위치는 DeliveryForm내로 지정됨
+                                                                                      // query를 통해 name이 zipcode1, zipcode2인 객체를 배열로 리턴함
+                      addressField = this.down('[name=address1]');                    // down메소드를 통해 name이 address1인 객체를 찾아 리턴함
+
+                  Ext.each(zipcodeField, function(field,idx) {                        // query로 통해 찾은 객체를 루핑돌며 우편번호값을 세팅함
+                    field.setValue(zipcode[idx]);
+                  });
+
+                  addressField.setValue(address);                                     // down으로 찾은 주소 텍스트 필드에 주소정보를 세팅함
+                },
+                scope: this
               }
             },
             {
@@ -112,11 +131,66 @@ Ext.define('ext5.view.chapter6.DeliveryForm',{
               margin: '0 5 0 5'
             },
             {
-              
+              xtype: 'textfield',
+              name: 'zipcode2',
+              width : 50,
+              margin: '0 5 0 5'
+            },
+            {
+              xtype: 'textfield',
+              name: 'address1',
+              flex: 1
+            }
+          ]
+        },
+        {
+          xtype: 'textfield',
+          columnWidth: 1,
+          name: 'address2',
+          margin: '0 0 5 85'
+        },
+        //배송요청정보
+        {
+          xtype: 'fieldcontainer',
+          fieldLabel: '배송요청일',
+          layout: 'hbox',
+          columnWidth: 1,
+          defaultType: 'textfield',
+          margin: '0 0 5 0',
+          items: [
+            {
+              xtype: 'datefield',
+              width: 100
+            },
+            {
+              xtype: 'label',
+              text: '~',
+              margin: '0 5 0 5'
+            },
+            {
+              xtype: 'datefield',
+              width: 100,
+              margin: '0 5 0 0'
+            },
+            {
+              xtype: 'datefield',
+              fieldLabel: '배송예정일',
+              width: 180
+            },
+            {
+              xtype: 'label',
+              text: '~',
+              margin: '0 5 0 5'
+            },
+            {
+              xtype: 'datefield',
+              width: 100,
+              margin: '0 5 0 0'
             }
           ]
         }
-      ]
+
+      ] // End DeliveryForm.items
     });
 
     this.callParent(arguments);
@@ -125,12 +199,48 @@ Ext.define('ext5.view.chapter6.DeliveryForm',{
     this.setLatestDelivery();
   },
 
-  resetDelivery: function(){
+  //새로운 배송지 선택시 이벤트
+  resetDelivery: function(radio, checked){
+    if(!checked) return;
+    var me = this,
+        delivery = ['zipcode1','zipcode2','address1','address2'];
 
+    Ext.each(delivery, function(field) {                            // 위에서 지정한 delivery배열 반복처리
+      me.down('textfield[name=' + field + ']').setValue();          // delivery배열값으로 textfield내 이름으로 검색하여 값을 초기화 함
+    });
   },
 
-  clickLatestDelivery: function(){
+  // 폼데이터 채우기는 예제(json name에 해당하는 값을 일괄적으로 세팅함)
+  clickLatestDelivery: function(raido, checked){
+    if(!checked) return;
 
+    var me = this;
+
+    //Case1 (첫번째벙법)
+    /*
+    this.ownerCt.getForm().load({                         // this.ownerCt는 DeliveryForm 클래스를 담은 부모 컨테이너를 의미함
+      url: '/resources/data/memberAddress.json',          // 호출할 서버 URL
+      params: {
+        addressnum: raido.inputValue                      // URL파라메터 정보(선택한 라디오버튼 값을 addressnum값으로 넘김)
+      }
+    });
+    */
+
+    //Case2,3은 Ext.Ajax.request를 이용하여 처리
+    Ext.Ajax.request({
+      url: '/resources/data/memberAddress.json', 
+      success: function (response) {
+        var response = Ext.decode(response.responseText);
+        //Case2 (두번째방법) - scope: this 설정을 반드시 해야함
+        //this.ownerCt.getForm().setValues(response.data);
+        //Case3 (세번째방법) - scope: this 설정을 반드시 해야함
+        //                  - model을 이용하여 맵핑처리
+        var model = Ext.create('ext5.model.smpl.CheckOut');
+        Ext.apply(model.data, response.data);
+        this.ownerCt.getForm().loadRecord(model);
+      },
+      scope: this
+    });
   },
 
   //동적인 Form을 구성하기 위한 Json로딩
