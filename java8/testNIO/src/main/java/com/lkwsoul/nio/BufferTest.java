6edcbf5fda5481d351fc0e9d84829876a38258c3
lkwsoul <lkwsoul@gmail.com>
2015-09-16 18:00:34 +0900

    Modify 20150916

--- java8/testNIO/src/main/java/com/lkwsoul/nio/BufferTest.java
+++ java8/testNIO/src/main/java/com/lkwsoul/nio/BufferTest.java
@@ -1,10 +1,7 @@
 package com.lkwsoul.nio;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.CharBuffer;
-import java.nio.IntBuffer;
+import java.nio.*;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,7 +20,8 @@ public class BufferTest {
 //    bean.test1();
 //    bean.test2();
 //    bean.test3();
-    bean.test4();
+//    bean.test4();
+    bean.test5();
   }
 
   /**
@@ -138,8 +136,83 @@ public class BufferTest {
     System.out.println("int  저장옹량 : " + intBuffer.capacity());
   }
 
+  /**
+   * - position : 현재 읽거나 쓰는 위치값이다. 인덱스 값이기 때문에 0부터 시작하며, limit 보다 큰 값을 가질 수 없다. 만약 position과 limit 값이 같아지면 더 이상 데이터를 쓰거나 읽을 수 없다는 뜻이 된다.
+   * - limit    : 버퍼에서 읽거나 쓸 수 있는 위치의 한계를 나타낸다. 이 값은 capacity 보다 작거나 같은 값을 가진다. 최초에 버퍼를 만들었을 때는 capacity와 같은 값을 가진다.
+   * - capacity : 버퍼의 최대 데이터 개수(메모리 크기)를 나타낸다. 인덱스 값이 아니라 수량임을 주의하자.
+   * - mark     : reset() 메소드를 실행했을 때 돌아오는 위치를 지정하는 인덱스로서 mark() 메소드로 지정할 수 있다. 주의할 점은 반드시 position 이하의 값으로 지정해주어야 한다. position이나 limit 의 값이 mark 값보다 작은 경우, mark 값은 자동 제거된다. mark가 없는 상태에서 reset() 메소드를 호출하면 InvalidMarkException이 발생한다.
+   *              mark는 position이나 limit이 mark 보다 작은 값으로 조정되면 자동적으로 없어집니다.
+   *
+   * - reset()  : position을 지정한 mark위치로 이동함
+   * - rewind() : position 위치를 0로 이동함
+   * - clear()  : limit는 capacity로 지정되고, position은 0으로, mark는 없어짐.(데이터는 삭제되지 않음)
+   * - compact(): 현재 position에서 limit 사이의 데이터가 0번 인덱스로 복사되고 현재 position은 복사된 데이터 다음 위치로 이동
+   *              호출하는 이유는 읽지 않은 데이터 뒤에 새로운 데이터를 저장하기 위해
+   *
+   * - put()    : 데이터 저장
+   * - get()    : 데이터 읽기
+   * - 데이터 저장/읽기에서 상대적(Relative), 절대적(Absolute)으로 구분함
+   *   상대적(Relative) : position값 증가
+   *   절대적(Absolute) : position값 증가되지 않음.
+   *
+   * - BufferOverflowException : position이 limit에 도달했을 때 put() 을 호출하면 발생
+   * - BufferUnderflowException: position이 limit에 도달했을 때 get() 을 호출하면 발생
+   * - InvalidMarkException    : mark가 없는 상태에서 reset() 메소드를 호출하면 발생
+   * - ReadOnlyBufferException : 읽기 전용 버퍼에서 put() 또는 compact() 메소드를 호출하면 발생
+   *
+   * - 크기 연관관계
+   *   0 <= mark <= position <= limit <= capacity
+   */
   public void test5(){
+    System.out.println("[7바이트 크기로 버퍼 생성]");
 
+    ByteBuffer buffer = ByteBuffer.allocateDirect(7);
+    printState(buffer);
+
+    buffer.put((byte) 10);
+    buffer.put((byte) 11);
+    System.out.println("[2바이트 저장후]");
+    printState(buffer);
+
+    buffer.put((byte) 12);
+    buffer.put((byte) 13);
+    buffer.put((byte) 13);
+    System.out.println("[3바이트 저장후]");
+    printState(buffer);
+
+    buffer.flip();
+    System.out.println("[flip 실행후]");
+    printState(buffer);
+
+    buffer.get(new byte[3]);
+    System.out.println("[3바이트 읽은후]");
+    printState(buffer);
+
+    buffer.mark();
+    System.out.println("[현재위치 mark 지정함]");
+
+    buffer.get(new byte[2]);
+    System.out.println("[2바이트 읽은후]");
+    printState(buffer);
+
+    buffer.reset();
+    System.out.println("[position을 mark 위치로 지정]");
+    printState(buffer);
+
+    buffer.rewind();
+    System.out.println("[rewind 실행후]");
+    printState(buffer);
+
+    buffer.clear();
+    System.out.println("[clear 실행후]");
+    printState(buffer);
+
+  }
+
+  public void printState(Buffer buffer) {
+    System.out.print("\tposition: " + buffer.position() + ", ");
+    System.out.print("\tlimit: " + buffer.limit() + ", ");
+    System.out.println("\tcapacity: " + buffer.capacity());
   }
 
 }
